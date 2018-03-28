d4c05222769b52d4de372c1b382a15a80fa7895c
jaf7 <janthonyfields@gmail.com>
2018-03-28 15:22:00 -0400

    merged distance matrix & point setting into promises & seperated concerns

--- src/init_map.js
+++ src/init_map.js
@@ -1,13 +1,14 @@
 // Load the map centered on a location, intialize classes for later use
-let map, geocoder, service, markers, bounds;
+let map, geocoder, placesService, distanceMatrixService, markersArray, bounds;
 function initMap() {
   map = new google.maps.Map(document.querySelector('#map'), {
     center: {lat: 40.7828647, lng: -73.9653551},
     zoom: 12
   })
   geocoder = new google.maps.Geocoder();
-  service = new google.maps.places.PlacesService(map)
+  distanceMatrixService = new google.maps.DistanceMatrixService
+  placesService = new google.maps.places.PlacesService(map)
   bounds = new google.maps.LatLngBounds()
   infoWindow = new google.maps.InfoWindow()
-  markers = []
+  markersArray = []
 }
