702af555665c4a2428c13e54d68e50cb6e18d03f
jaf7 <janthonyfields@gmail.com>
2018-03-29 09:16:13 -0400

    add html generation to generate_best_destination

--- src/generate_best_destination.js
+++ src/generate_best_destination.js
@@ -1,11 +1,17 @@
-let percent1 = 0.3125
-let percent2 = 1 - percent1
-
 function generateBestDestination(response) {
   let originList = response.originAddresses;
   let destinationList = response.destinationAddresses; 
   let outputDiv = document.getElementById('output');
   outputDiv.innerHTML = '';
+
+  for (var i = 0; i < originList.length; i++) {
+    var results = response.rows[i].elements;
+    for (var j = 0; j < results.length; j++) {
+      outputDiv.innerHTML += originList[i] + ' to ' + destinationList[j] +
+          ': ' + results[j].distance.text + ' in ' +
+          results[j].duration.text + '<br>';
+    }
+  }
 
   geoCodeAddress(destinationList[0])
   .then(response => { // this response should have LatLng at response[0].geometry.location
